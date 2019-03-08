d3beb7238bdb37ba2c7219e353f3065c5c081646
ceky <cekyy07@gmail.com>
2019-03-08 21:57:22 +0200

    Style the create button

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,19 +1,25 @@
+import { withRouter } from 'react-router-dom';
 import React, { Component } from 'react';
 import './index.css';
 
 
 class Intro extends Component {
+
+  onClickExplore = () => {
+    this.props.history.push('/movies');
+  };
+
   render() {
     return (
       <div className="intro-container">
         <img src="assets/bg.jpg"></img>
         <div className="main-text-container">
           <h1>I set myself on a journey of watching 100 documentaries in 2019</h1>
-          <a className="explore-button" href="/movies">Explore</a>
+          <button className="explore-button" onClick={this.onClickExplore}>Explore</button>
         </div>        
       </div>
     );
   }
 }
 
-export default Intro;
+export default withRouter(Intro);
